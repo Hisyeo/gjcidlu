9167bfea5362caa41a317d6316c2d78fe5577a1c
MadcapJake <madcap.russo@gmail.com>
2025-11-13 06:20:05 -0600

    Add prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     distDir: 'gen',
+    basePath: isProd ? '/gjcidlu' : undefined,
+    assetPrefix: isProd ? '/gjcidlu/' : undefined,
 };
 
 export default nextConfig;

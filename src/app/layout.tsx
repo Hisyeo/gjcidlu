1c21e510991889920f30ad29a4b13e8845ced078
MadcapJake <madcap.russo@gmail.com>
2025-11-17 22:19:18 -0600

    Add a simple footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,12 @@ export default function RootLayout({
           <SettingsProvider> {/* Add SettingsProvider */}
             <ClientWrapper allEntries={entriesData}>
               {children}
+              <footer className="bg-gray-100 text-center text-gray-600 text-sm p-4 mt-8">
+                © 2025 Copyright:{" "}
+                <a href="https://hisyeo.github.io" className="text-blue-600 hover:underline">
+                  Hîsyêô Institute
+                </a>
+              </footer>
             </ClientWrapper>
           </SettingsProvider>
         </AppProvider>
